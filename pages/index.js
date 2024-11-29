13e4ea3657761aeb9dd7e313664a5fe868d40b36
Sérgio Ramos <srramos123@gmail.com>
2024-11-29 19:04:57 +0000

    Inclusão de h3

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
     return<h1>Jesus Cristo é o meu salvador.</h1>
-        <h3>Ele morreu pelos meus pecados e no terceiro dia ressucitou</h3>
+    <h3>Ele morreu pelos meus pecados e no terceiro dia ressucitou</h3>
 }
 
 export default Home;
